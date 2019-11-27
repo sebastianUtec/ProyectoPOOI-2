c7b7569dc772a603a4cf96fa61a21c952f288051
sebastianUtec <56973797+sebastianUtec@users.noreply.github.com>
2019-11-26 19:08:17 -0500

    Create CObjeto.cpp

---
+++ CObjeto.cpp
@@ -0,0 +1,11 @@
+//
+// Created by SEBASTIAN on 26/11/2019.
+//
+
+#include "CObjeto.h"
+CObjeto::CObjeto(int x, int y, char caracter) {
+    posicionx=x;
+    posiciony=y;
+    this->caracter=caracter;
+
+}
